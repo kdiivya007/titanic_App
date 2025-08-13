4e9bcc76880040f7a9449d134d54a8b9618791ea
Divya sree Kondapalli <divya.k@feuji.com>
2025-08-13 19:27:12 +0530

    updated new file

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ sex_port=list(df['Sex'].unique())
 col1,col2=st.columns(2)
 selected_port=col1.selectbox(label='selected_port',options=embarked_port)
 selected_gender=col2.selectbox('selected_geander',options=sex_port)
-df_plot=df[df['Embarked']==embarked_port]
-df_plot=df_plot['Sex']==sex_port
+df_plot=df[df['Embarked']==selected_port]
+df_plot=df_plot[df_plot['Sex']==selected_gender]
