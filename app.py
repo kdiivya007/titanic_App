2cbf1d7b6725b10114730e4cc79f03b380b1ceac
Divya sree Kondapalli <divya.k@feuji.com>
2025-08-13 20:04:49 +0530

    updated new file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
 st.title('deploy new app')
 df=pd.read_csv('titani_data.csv')
 df['Embarked']=df['Embarked'].fillna('Unknown')
@@ -19,5 +20,16 @@ col1.plotly_chart(plot)
 
 df_plot_pie=df_plot.loc[:,['PassengerId','Survived']].groupby(['Survived']).count().reset_index()
 df_plot_pie.rename({'PassengerId':'count of passengers survived'},axis='columns',inplace=True)
-pie_plot=px.pie(data_frame=df_plot_pie,template='seaborn',title='count of pass survided',values='count of passengers',names='Survived')
+print(df_plot_pie.columns)
+print(df_plot_pie.head())
+
+# pie_plot=px.pie(data_frame=df_plot_pie,template='seaborn',title='count of pass survided',values='count of passengers',names='Survived')
+pie_plot = px.pie(
+    data_frame=df_plot_pie,
+    template='seaborn',
+    title='Count of passengers survived',
+    values='count of passengers survived',  # <-- match the actual column name
+    names='Survived'
+)
+
 col2.plotly_chart(pie_plot)
